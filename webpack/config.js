1a477f35e381757216802462f0f14deba8a345b9
Mrparkers <mrparkers1067@gmail.com>
2016-11-13 20:37:45 -0600

    Add webpack-dev-server for demonstration

---
+++ webpack/config.js
@@ -0,0 +1,40 @@
+const path = require('path'),
+    paths = {
+        out: path.join(__dirname, '..', 'public'),
+        js: path.join(__dirname, '..', 'src', 'index.js'),
+        html: path.join(__dirname, 'index.html')
+    };
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        javascript: paths.js,
+        html: paths.html
+    },
+    output: {
+        filename: 'bundle.js',
+        path: paths.out
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css']
+            },
+            {
+                test: /\.html$/,
+                loader: 'file?name=[name].[ext]'
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+        inline: true,
+        historyApiFallback: true
+    }
+};
