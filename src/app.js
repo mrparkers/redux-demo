1274cae1e73912a96904461bd10996a9261b4ece
Mrparkers <mrparkers1067@gmail.com>
2016-11-13 20:51:35 -0600

    Add Header for demo app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const renderDisplayedComponent = children => React.Children.map(children, child => React.cloneElement(child));
+
+const Header = () => (
+    <nav className="header ui fixed menu">
+        <div className="ui container">
+            <Link to="/" className="header item">Redux - Monday Night Meeting</Link>
+            <Link to="/" className="item">Simple Redux Demo</Link>
+            <Link to="/" className="item">Todo List (Stateful React Components)</Link>
+            <Link to="/" className="item">Todo List (Redux)</Link>
+        </div>
+    </nav>
+);
 
 export default class App extends React.Component {
     render() {
@@ -8,6 +20,7 @@ export default class App extends React.Component {
 
         return (
             <div className="app">
+                <Header />
                 {renderDisplayedComponent(children)}
             </div>
         );
